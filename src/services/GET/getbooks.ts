a5c85950bfb4298cf0872d197542c28e9aa985d8
airtonsena10 <airtonsena41@gmail.com>
2023-08-03 16:24:54 -0300

    axios and api do google , getbooks

---
+++ src/services/GET/getbooks.ts
@@ -0,0 +1,121 @@
+import { API } from '../_api'
+
+export interface VolumeBooks {
+  kind: string
+  totalItems: number
+  items: Item[]
+}
+
+export interface Item {
+  kind: Kind
+  id: string
+  etag: string
+  selfLink: string
+  volumeInfo: VolumeInfo
+  searchInfo?: SearchInfo
+}
+
+export enum AccessViewStatus {
+  FullPublicDomain = 'FULL_PUBLIC_DOMAIN',
+  Sample = 'SAMPLE',
+}
+
+export enum Country {
+  Br = 'BR',
+}
+
+export enum TextToSpeechPermission {
+  Allowed = 'ALLOWED',
+}
+
+export enum Viewability {
+  AllPages = 'ALL_PAGES',
+  Partial = 'PARTIAL',
+}
+
+export enum Kind {
+  BooksVolume = 'books#volume',
+}
+
+export interface SaleInfoListPrice {
+  amount: number
+  currencyCode: CurrencyCode
+}
+
+export enum CurrencyCode {
+  Brl = 'BRL',
+}
+
+export enum Saleability {
+  ForSale = 'FOR_SALE',
+  Free = 'FREE',
+  NotForSale = 'NOT_FOR_SALE',
+}
+
+export interface SearchInfo {
+  textSnippet: string
+}
+
+export interface VolumeInfo {
+  title: string
+  subtitle?: string
+  authors: string[]
+  description?: string
+  industryIdentifiers: IndustryIdentifier[]
+  readingModes: ReadingModes
+  pageCount: number
+  printType: PrintType
+  allowAnonLogging: boolean
+  contentVersion: string
+  panelizationSummary: PanelizationSummary
+  imageLinks: ImageLinks
+  language: Language
+}
+
+export interface ImageLinks {
+  smallThumbnail: string
+  thumbnail: string
+}
+
+export interface IndustryIdentifier {
+  type: Type
+  identifier: string
+}
+
+export enum Type {
+  Isbn10 = 'ISBN_10',
+  Isbn13 = 'ISBN_13',
+  Other = 'OTHER',
+}
+
+export enum Language {
+  En = 'en',
+}
+
+export enum MaturityRating {
+  NotMature = 'NOT_MATURE',
+}
+
+export interface PanelizationSummary {
+  containsEpubBubbles: boolean
+  containsImageBubbles: boolean
+}
+
+export enum PrintType {
+  Book = 'BOOK',
+}
+
+export interface ReadingModes {
+  text: boolean
+  image: boolean
+}
+
+export const getbooks = async (search: string) => {
+  try {
+    const data = await API.get<VolumeBooks>(`volumes?q=${search}`)
+    return data
+  } catch (error) {
+    console.log('ERROR getbooks ----> ', error)
+    return null
+  }
+}
---
+++ src/services/GET/getbooksById.ts
@@ -0,0 +1,72 @@
+import { API } from '../_api'
+
+export interface Book {
+  kind: string
+  id: string
+  etag: string
+  selfLink: string
+  volumeInfo: VolumeInfo
+  saleInfo: SaleInfo
+}
+
+export interface Epub {
+  isAvailable: boolean
+}
+
+export interface PDF {
+  isAvailable: boolean
+  acsTokenLink: string
+}
+
+export interface SaleInfo {
+  country: string
+  saleability: string
+  isEbook: boolean
+}
+
+export interface VolumeInfo {
+  title: string
+  authors: string[]
+  description: string
+  readingModes: ReadingModes
+  pageCount: number
+  printedPageCount: number
+  dimensions: Dimensions
+  printType: string
+  contentVersion: string
+  imageLinks: ImageLinks
+  language: string
+}
+
+export interface Dimensions {
+  height: string
+  width: string
+  thickness: string
+}
+
+export interface ImageLinks {
+  smallThumbnail: string
+  thumbnail: string
+  small: string
+  medium: string
+}
+
+export interface IndustryIdentifier {
+  type: string
+  identifier: string
+}
+
+export interface ReadingModes {
+  text: boolean
+  image: boolean
+}
+
+export const getbooksById = async (id: string) => {
+  try {
+    const data = await API.get<Book>(`volumes/${id}`)
+    return data
+  } catch (error) {
+    console.log('ERROR getbooksById ----> ', error)
+    return null
+  }
+}
