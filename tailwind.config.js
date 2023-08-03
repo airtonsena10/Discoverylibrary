fdc5a2bc9022adfc2415774077f942c8c63df097
airtonsena10 <airtonsena41@gmail.com>
2023-08-03 16:20:03 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      width: {
+        120: '7.5rem',
+      },
+      height: {
+        200: '12.5rem',
+        '17.5rem': '17.5rem',
+      },
+      minWidth: {
+        '20rem': '20rem',
+        120: '7.5rem',
+      },
+    },
+  },
+  plugins: [],
+}
